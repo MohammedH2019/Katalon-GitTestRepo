bfab062c0057a873f9562d27551682f15c6c58c6
stoppound <60070143@kmitl.ac.th>
2021-02-24 11:42:53 +0700

    case

---
+++ Object Repository/P_USE/26/Page_Bluuu/i__far fa-edit.rs
@@ -0,0 +1,54 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>i__far fa-edit</name>
+   <tag></tag>
+   <elementGuidId>79527a9a-dafa-4df8-840f-10ed43729e0b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>i.far.fa-edit</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//fieldset[@id='__BVID__169']/div/div/div[2]/button/i</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>i</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>far fa-edit</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;__BVID__169&quot;)/div[@class=&quot;bv-no-focus-ring&quot;]/div[@class=&quot;input-group&quot;]/div[@class=&quot;input-group-append&quot;]/button[@class=&quot;btn btn-primary&quot;]/i[@class=&quot;far fa-edit&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//fieldset[@id='__BVID__169']/div/div/div[2]/button/i</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//i</value>
+   </webElementXpaths>
+</WebElementEntity>
--- Scripts/Special Case/26_Phone_OTP/Script1614060519033.groovy
+++ Scripts/Special Case/26_Phone_OTP/Script1614060519033.groovy
@@ -16,4 +16,57 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
+import com.kms.katalon.core.testdata.InternalData as InternalData
+
+InternalData data = findTestData('id_fb')
+
+for (def index : (0..data.getRowNumbers() - 1)) {
+    WebUI.openBrowser('')
+
+    WebUI.navigateToUrl('https://uat.bluuu.co/')
+
+    WebUI.maximizeWindow()
+
+    WebUI.click(findTestObject('Object Repository/P_USE/26/Page_Bluuu -/img_TH_avatar order-3 order-xl-4 ml-4 mt-1'))
+
+    WebUI.click(findTestObject('Object Repository/P_USE/26/Page_Bluuu -/button_ Facebook'))
+
+    WebUI.switchToWindowTitle('Facebook')
+
+    WebUI.setText(findTestObject('Used/Page-Facebook/input__email'), data.internallyGetValue('id', index))
+
+    WebUI.setText(findTestObject('Object Repository/P_USE/26/Page_Facebook/input__pass'), data.internallyGetValue('pass', 
+            index))
+
+    WebUI.click(findTestObject('Object Repository/P_USE/26/Page_Facebook/label__loginbutton'))
+
+    WebUI.delay(3)
+        
+    WebUI.click(findTestObject('Object Repository/P_USE/26/Page_ Facebook/button_ Linda'))
+	
+    WebUI.delay(4)
+
+    WebUI.switchToWindowTitle('Bluuu - จ้างแม่บ้าน ผู้เชี่ยวชาญเรื่องบ้านที่คุณไว้วางใจได้มากที่สุด')
+
+    WebUI.click(findTestObject('Object Repository/P_USE/26/Page_Bluuu -/img_TH_avatar'))
+
+    WebUI.click(findTestObject('Object Repository/P_USE/26/Page_Bluuu -/a_'))
+
+    WebUI.click(findTestObject('Object Repository/P_USE/26/Page_Bluuu/i__far fa-edit'))
+
+    WebUI.setText(findTestObject('Object Repository/P_USE/26/Page_Bluuu/input____BVID__230'), '0999999999')
+
+    WebUI.click(findTestObject('P_USE/26/Page_Bluuu/button_'))
+
+    WebUI.delay(60)
+
+    WebUI.setText(findTestObject('Object Repository/P_USE/26/Page_Bluuu/input_ 6 ___BVID__233'), '999999')
+
+    WebUI.click(findTestObject('P_USE/26/Page_Bluuu/button_'))
+
+    //	phone = WebUI.getAttribute(findTestObject('Object Repository/P_USE/26/Page_Bluuu/input____BVID__170'), 'Value')
+    //
+    //    System.out.println(phone)
+    WebUI.delay(20)
+}
 
